d0ad9552021cb8599929df809ec35b7d9f03a62c
taylortreece <tayloratreece@gmail.com>
2021-08-05 15:00:27 -0500

    commit tree c26b5be38821194a8e2bf39d340719efb24949af

--- index.js
+++ index.js
@@ -4,16 +4,35 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, iteratee) {
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
 
+      for(const value of newCollection) {
+        iteratee(value)
+      }
+
+      return collection
     },
 
-    map: function() {
+    map: function(collection, iteratee) {
+      let newArray = []
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
 
+      for(const value of newCollection) {
+        newArray.push(iteratee(value))
+      }
+
+      return newArray
     },
 
-    reduce: function() {
+    reduce: function(collection, iteratee) {
+      let newValue;
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
+      console.log(newCollection)
 
+      for(const value of newCollection) {
+        console.log(iteratee(accumulator, value))
+      }
     },
 
     functions: function() {
